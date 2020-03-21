394458bcbd851d28040c429d995e04801c87b1f6
sydykov77 <erjansydykov77>
2020-03-21 22:49:26 +0630

    ат

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,34 +5,55 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        String key=new String();
-        Map<String, String[]> bigDictionary = new HashMap<>();
+        Map<String, String[]> dictionary = new HashMap<>();
+        Map<String, String[]> bigВictionary = new HashMap<>();
 
-        bigDictionary.put("красивый", new String[]{"симпотичный", "эффектный", "роскошный", "благовидный","картинный","бесподобный","хорошенький","великолепный","изящный"});
-        bigDictionary.put("дом", new String[]{"здание", "убежище", "хата", "хижина","этажка","лачуга","вилла","дача","усадьба"});
-        bigDictionary.put("человек", new String[]{"лицо", "личность", "гражданин", "индивид","адам","смертный","индивидуальность","персона","особа"});
-        bigDictionary.put("большой", new String[]{"большуший", "огромный", "громадный", "внушительный","изрядный","объемистый","заметный","ощутительный","колоссальный"});
+        dictionary.put("красивый", new String[]{"симпотичный", "эффектный", "роскошный", "благовидный","картинный","бесподобный","хорошенький","великолепный","изящный"});
+        dictionary.put("дом", new String[]{"здание", "убежище", "хата", "хижина","этажка","лачуга","вилла","дача","усадьба"});
+        dictionary.put("человек", new String[]{"лицо", "личность", "гражданин", "индивид","адам","смертный","индивидуальность","персона","особа"});
+        dictionary.put("большой", new String[]{"большуший", "огромный", "громадный", "внушительный","изрядный","объемистый","заметный","ощутительный","колоссальный"});
 
 
-        while (true) {
-            System.out.println("Введите слово:");
-            Scanner scanner = new Scanner(System.in);
-            String sentences = scanner.nextLine();
-            String[] words = sentences.split(" ");
-            for (String word : words) {
-                String[] sinonyms = bigDictionary.get(word);
-                Random r = new Random();
-                if (sinonyms != null) {
-                    System.out.print(sinonyms[r.nextInt(sinonyms.length)] + " ");
-                }else if (word != null){
-                    System.out.print(word + " ");
-                }
-                else {
-                    System.out.print("другое слово");
-                }
+        Set<String> keys = dictionary.keySet();
+        Iterator iter = keys.iterator();
+        while (iter.hasNext()) {
+            String key = iter.next().toString();
+            String[] values = dictionary.get(key);
+            bigВictionary.put(key, values);
+            for (int i =0; i<values.length;i++) {
+                String budushiyKlyuch = values[i];
+                ArrayList<String> strArr = new ArrayList<>(values.length);
+                strArr.addAll(Arrays.asList(values));
+                strArr.remove(budushiyKlyuch);
+                strArr.add(key);
+                String[] newVals = new String[strArr.size()];
+                newVals = strArr.toArray(newVals);
+                System.out.println(budushiyKlyuch
+                        + " - " + Arrays.toString(newVals));
+                bigВictionary.put(budushiyKlyuch, newVals);
             }
-            System.out.println();
         }
+
+
+        System.out.println("Введите слово: ");
+
+        Scanner s = new Scanner(System.in);
+        String sentences = s.nextLine();
+        String[] words = sentences.split(" ");
+        Random r = new Random();
+
+        for (int i = 0; i < words.length; i++) {
+            String[] synonyms = dictionary.get(words[i]);
+            if (synonyms != null) {
+                int randomN = r.nextInt(synonyms.length);
+
+                System.out.printf(synonyms[randomN] + " ");
+
+            } else {
+                System.out.println("Не понял Вас");
+            }
+
+        }
+
     }
 }
-
